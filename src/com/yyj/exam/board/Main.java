3d4345d6813f502a69a67fa16f21f11ea3dd5bed
tlsrb0025 <tlsrb0025@gmail.com>
2021-11-16 12:45:36 +0900

    두번째 수정

--- src/com/yyj/exam/board/Main.java
+++ src/com/yyj/exam/board/Main.java
@@ -1,12 +1,19 @@
 package com.yyj.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
     // write your code here
+
+    Scanner scan = new Scanner(System.in);
+
       System.out.println("==게시판 v 0.1==");
       System.out.println("==프로그램 시작==");
-      System.out.println("명령어 입력)");
+      System.out.print("명령어 입력)");
+      String a = scan.next();
+    System.out.println("입력된 명령어 : " + a);
       System.out.println("==프로그램 종료==");
   }
 }
