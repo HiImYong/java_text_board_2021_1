2a9cc40e864e55bc9a8fb8cabbfc0027aadc8734
tlsrb0025 <tlsrb0025@gmail.com>
2021-11-17 09:58:24 +0900

    11월 17일 1차수정

--- src/com/yyj/exam/board/Main.java
+++ src/com/yyj/exam/board/Main.java
@@ -12,12 +12,26 @@ public class Main {
       System.out.println("==게시판 v 0.1==");
       System.out.println("==프로그램 시작==");
     while(true){
-      System.out.printf("명령어 입력) ");
-      String a = scan.next();
+      System.out.printf("명령) ");
+      String cmd = scan.next();
 
-        if(a.equals("exit")){
+        if(cmd.equals("/usr/article/write")){
+          System.out.println("- 게시물 등록 -");
+          System.out.print("제목 : ");
+          String title = scan.next();
+          System.out.print("내용 : ");
+          String body = scan.next();
+          int id=1;
+          System.out.printf("%d번 게시물이 등록되었습니다.\n",id);
+        }
+
+        else if(cmd.equals("exit")){
           break;
         }
+
+        else {
+        System.out.printf("입력된 명령어 : %s\n",cmd);
+      }
       }
       System.out.println("==프로그램 종료==");
 
