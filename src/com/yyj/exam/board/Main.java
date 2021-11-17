01d2d52a2fa968473c637d6207101f0fd0171a66
tlsrb0025 <tlsrb0025@gmail.com>
2021-11-17 13:10:50 +0900

    21년 11월 17일 수업 마무리

--- src/com/yyj/exam/board/Main.java
+++ src/com/yyj/exam/board/Main.java
@@ -9,8 +9,8 @@ public class Main {
     // write your code here
 
     Scanner scan = new Scanner(System.in); //시스템인 : 키보드를 받아낸다
+    ArrayList<Article> articles = new ArrayList<Article>();
     int articlesLastId = 0;
-
 
     System.out.println("==게시판 v 0.1==");
     System.out.println("==프로그램 시작==");
@@ -27,17 +27,29 @@ public class Main {
         articlesLastId = articlesLastId + 1;
         int tempId = articlesLastId;
 
-
-        Aritcle article = new Aritcle(tempId, title, body);
-
+        Article article = new Article(tempId, title, body);
+        articles.add(article);
 
         System.out.println("생성된 게시물 객체 : " + article.toString()); //
 
-
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+      }
+
+      else if (cmd.equals("/usr/article/detail")) {
 
 
-      } else if (cmd.equals("exit")) {
+        System.out.printf("%d번 게시물을 출력합니다.\n", articlesLastId);
+
+        Article A = articles.get(articlesLastId-1);
+        System.out.printf("id : %d ",A.getId());
+        System.out.printf("제목 : %s ", A.getTitle());
+        System.out.printf("내용 : %s\n", A.getBody());
+
+      }
+
+
+
+      else if (cmd.equals("exit")) {
         break;
       } else {
         System.out.printf("입력된 명령어 : %s\n", cmd);
@@ -50,18 +62,33 @@ public class Main {
 }
 
 
-class Aritcle {
+class Article {
 
   int id;
   String title;
   String body;
 
 
-  public Aritcle(int id, String title, String body) {
+  public Article(int id, String title, String body) {
     this.id = id;
     this.title = title;
     this.body = body;
 
+  }
+
+
+  public int getId(){
+    return id;
+  }
+
+
+  public String getTitle(){
+    return title;
+  }
+
+
+  public String getBody(){
+    return body;
   }
 
   @Override
