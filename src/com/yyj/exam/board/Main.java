c22b91b5b2c7b76fcef25da72ccf117b26265855
tlsrb0025 <tlsrb0025@gmail.com>
2021-11-17 10:53:04 +0900

    toString 오버라이드

--- src/com/yyj/exam/board/Main.java
+++ src/com/yyj/exam/board/Main.java
@@ -12,56 +12,64 @@ public class Main {
     int articlesLastId = 0;
 
 
-      System.out.println("==게시판 v 0.1==");
-      System.out.println("==프로그램 시작==");
-    while(true){
+    System.out.println("==게시판 v 0.1==");
+    System.out.println("==프로그램 시작==");
+    while (true) {
       System.out.printf("명령) ");
       String cmd = scan.next();
 
-        if(cmd.equals("/usr/article/write")){
-          System.out.println("- 게시물 등록 -");
-          System.out.print("제목 : ");
-          String title = scan.next();
-          System.out.print("내용 : ");
-          String body = scan.next();
-          articlesLastId = articlesLastId+1;
-          int tempId = articlesLastId;
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println("- 게시물 등록 -");
+        System.out.print("제목 : ");
+        String title = scan.next();
+        System.out.print("내용 : ");
+        String body = scan.next();
+        articlesLastId = articlesLastId + 1;
+        int tempId = articlesLastId;
 
 
-          Aritcle article = new Aritcle();
-          article.id = tempId;
-          article.title = title;
-          article.body = body;
-
-          System.out.println("생성된 게시물 객체 : " + article);
+        Aritcle article = new Aritcle(tempId, title, body);
 
 
+        System.out.println("생성된 게시물 객체 : " + article.toString()); //
 
-          System.out.printf("%d번 게시물이 등록되었습니다.\n",article.id);
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
 
 
-        }
-
-        else if(cmd.equals("exit")){
-          break;
-        }
-
-        else {
-        System.out.printf("입력된 명령어 : %s\n",cmd);
+      } else if (cmd.equals("exit")) {
+        break;
+      } else {
+        System.out.printf("입력된 명령어 : %s\n", cmd);
       }
-      }
-      System.out.println("==프로그램 종료==");
+    }
+    System.out.println("==프로그램 종료==");
 
-      scan.close();
+    scan.close();
   }
 }
 
 
-class Aritcle{
+class Aritcle {
 
   int id;
   String title;
   String body;
 
 
+  public Aritcle(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+
+  }
+
+  @Override
+  public String toString() {
+    return "게시글{" +
+        "id=" + id +
+        ", 제목='" + title + '\'' +
+        ", 내용='" + body + '\'' +
+        '}';
+  }
 }
